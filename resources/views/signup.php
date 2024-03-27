a2105a9cc1ce42161f07873a642f5ec7e5e804d8
VieetKhooii <nguyenkhooii03@gmail.com>
2024-03-27 23:52:22 +0700

    Register Ajax

--- resources/views/signup.php
+++ resources/views/signup.php
@@ -1,11 +1,12 @@
-<link rel="stylesheet" href="../css/signup.css">
+<link rel="stylesheet" href="/css/signup.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<div id="form_signUp" onsubmit="validateSignUp(event)">
-    <form onclick="validateForm()" >
+<div id="form_signUp">
+    <form id="signupForm" method="post">
         <h2>Đăng ký</h2>
-        <label for="fullname">Họ và tên:</label>
-        <input type="text" id="fullname" name="fullname" required oninvalid="this.setCustomValidity('Vui lòng nhập Họ và tên.')" oninput="this.setCustomValidity('')">
+        <label for="full_name">Họ và tên:</label>
+        <input type="text" id="full_name" name="full_name" required oninvalid="this.setCustomValidity('Vui lòng nhập Họ và tên.')" oninput="this.setCustomValidity('')">
         <label for="phone">Số điện thoại:</label>
         <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required oninvalid="this.setCustomValidity('Vui lòng nhập Số điện thoại 10 chữ số.')" oninput="this.setCustomValidity('')">
         <label for="email">Email:</label>
@@ -15,20 +16,26 @@
         <div class="form-row">
             <div>
                 <label for="dob">Ngày sinh:</label>
-                <input type="date" id="dob" name="dob" required oninvalid="this.setCustomValidity('Vui lòng chọn Ngày sinh.')" oninput="this.setCustomValidity('')">
+                <input type="date" id="date_of_birth" name="date_of_birth" required oninvalid="this.setCustomValidity('Vui lòng chọn Ngày sinh.')" oninput="this.setCustomValidity('')">
             </div>
             <div>
                 <label for="gender" id="lb_gender">Giới tính:</label>
                 <select id="gender" name="gender" required oninvalid="this.setCustomValidity('Vui lòng chọn Giới tính.')" oninput="this.setCustomValidity('')">
                     <option value="male">Nam</option>
                     <option value="female">Nữ</option>
+                    <option value="cat">Mèo</option>
+                    <option value="dog">Chó</option>
+                    <option value="shark">Cá mập</option>
+                    <option value="helicopter">Trực thăng</option>
+                    <option value="non-tax">Người không phải đóng thuế</option>
+                    <option value="no">hong thích có giới tính</option>
                 </select>
             </div>
         </div>
         </option>
         </select>
-        <br><label for="region">Khu vực:</label>
-        <input type="text" id="region" name="region" required oninvalid="this.setCustomValidity('Vui lòng chọn Khu vực.')" oninput="this.setCustomValidity('')">
+        <br><label for="address">Địa chỉ:</label>
+        <input type="text" id="address" name="address" required oninvalid="this.setCustomValidity('Vui lòng nhập địa chỉ.')" oninput="this.setCustomValidity('')">
         <label for="captcha">Mã captcha:</label>
         <div id="captchaBackground" style="display: flex;">
             <input type="text" id="captcha" name="captcha" required oninvalid="this.setCustomValidity('Vui lòng nhập mã captcha.')" oninput="this.setCustomValidity('')">
@@ -36,7 +43,7 @@
             <button type="button" id="refreshButton" onclick="refreshCaptcha()"><i class="fa-solid fa-rotate-right"></i></button>
         </div>
         <small>Nhập mã gatcha theo hình bên phải. <span id="Notification"></span></small>
-        <input type="submit" value="Đăng ký" onclick="validateCaptcha()">
+        <input type="submit" value="Đăng ký">
     </form>
 </div>
-<script src="../js/app.js"></script>
+<script src="/js/app.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -49,70 +49,64 @@ Route::middleware(['jwt.attach'])->group(function () {
     Route::resource('users', UserController::class);
     
     //Seat Type Routes
-Route::get('seatTypes/customerget', [SeatTypeController::class,'getAllSeatTypesForCustomer']);
-Route::resource('seatTypes', SeatTypeController::class);
-Route::post('seatTypes/search', [SeatTypeController::class, 'search']);
-Route::put('seatTypes/hide/{id}', [SeatTypeController::class, 'hide']);
-//Seat Routes
-Route::get('seats/customerget', [SeatController::class,'getAllSeatsForCustomer']);
-Route::resource('seats', SeatController::class);
-Route::post('seats/search', [SeatController::class,'search']);
-Route::put('seats/hide/{id}', [SeatController::class,'hide']);
-//Category Routes
-Route::get('categories/customerget', [CategoryController::class,'getAllCategoriesForCustomer']);
-Route::resource('categories', CategoryController::class);
-Route::post('categories/search', [CategoryController::class,'search']);
-Route::put('categories/hide/{id}', [CategoryController::class,'hide']);
-//Combo Routes
-Route::get('combos/customerget', [ComboController::class,'getAllCombosForCustomer']);
-Route::resource('combos',ComboController::class);
-Route::post('combos/search', [ComboController::class,'search']);
-Route::put('combos/hide/{id}', [ComboController::class,'hide']);
-//Consume Routes
-Route::get('consumes/customerget', [ConsumeController::class,'getAllConsumesForCustomer']);
-Route::resource('consumes',ConsumeController::class);
-Route::post('consumes/search', [ConsumeController::class,'search']);
-Route::put('consumes/hide/{id}', [ConsumeController::class,'hide']);
-//Movie Routes
-Route::get('movies/customerget', [MovieController::class,'getAllMoviesForCustomer']);
-Route::resource('movies',MovieController::class);
-Route::post('movies/search', [MovieController::class,'search']);
-Route::put('movies/hide/{id}', [MovieController::class,'hide']);
-//Room Routes
-Route::get('rooms/customerget', [RoomController::class,'getAllRoomsForCustomer']);
-Route::resource('rooms',RoomController::class);
-Route::post('rooms/search', [RoomController::class,'search']);
-Route::put('rooms/hide/{id}', [RoomController::class,'hide']);
-//Showtime Routes
-Route::get('showtimes/customerget', [ShowtimeController::class,'getAllShowtimesForCustomer']);
-Route::resource('showtimes',ShowtimeController::class);
-Route::post('showtimes/search', [ShowtimeController::class,'search']);
-Route::put('showtimes/hide/{id}', [ShowtimeController::class,'hide']);
-//Transaction Routes
-Route::get('transactions/customerget', [TransactionController::class,'getAllTransactionsForCustomer']);
-Route::resource('transactions',TransactionController::class);
-Route::post('transactions/search', [TransactionController::class,'search']);
-Route::put('transactions/hide/{id}', [TransactionController::class,'hide']);
-//Reservation Routes
-Route::get('reservations/customerget', [ReservationController::class,'getAllReservationsForCustomer']);
-Route::resource('reservations',ReservationController::class);
-Route::post('reservations/search', [ReservationController::class,'search']);
-Route::put('reservations/hide/{id}', [ReservationController::class,'hide']);
-// Auth::routes();
-Route::resource('consume', ConsumeController::class);
+    Route::get('seatTypes/customerget', [SeatTypeController::class,'getAllSeatTypesForCustomer']);
+    Route::resource('seatTypes', SeatTypeController::class);
+    Route::post('seatTypes/search', [SeatTypeController::class, 'search']);
+    Route::put('seatTypes/hide/{id}', [SeatTypeController::class, 'hide']);
+    //Seat Routes
+    Route::get('seats/customerget', [SeatController::class,'getAllSeatsForCustomer']);
+    Route::resource('seats', SeatController::class);
+    Route::post('seats/search', [SeatController::class,'search']);
+    Route::put('seats/hide/{id}', [SeatController::class,'hide']);
+    //Category Routes
+    Route::get('categories/customerget', [CategoryController::class,'getAllCategoriesForCustomer']);
+    Route::resource('categories', CategoryController::class);
+    Route::post('categories/search', [CategoryController::class,'search']);
+    Route::put('categories/hide/{id}', [CategoryController::class,'hide']);
+    //Combo Routes
+    Route::get('combos/customerget', [ComboController::class,'getAllCombosForCustomer']);
+    Route::resource('combos',ComboController::class);
+    Route::post('combos/search', [ComboController::class,'search']);
+    Route::put('combos/hide/{id}', [ComboController::class,'hide']);
+    //Consume Routes
+    Route::get('consumes/customerget', [ConsumeController::class,'getAllConsumesForCustomer']);
+    Route::resource('consumes',ConsumeController::class);
+    Route::post('consumes/search', [ConsumeController::class,'search']);
+    Route::put('consumes/hide/{id}', [ConsumeController::class,'hide']);
+    //Movie Routes
+    Route::get('movies/customerget', [MovieController::class,'getAllMoviesForCustomer']);
+    Route::resource('movies',MovieController::class);
+    Route::post('movies/search', [MovieController::class,'search']);
+    Route::put('movies/hide/{id}', [MovieController::class,'hide']);
+    //Room Routes
+    Route::get('rooms/customerget', [RoomController::class,'getAllRoomsForCustomer']);
+    Route::resource('rooms',RoomController::class);
+    Route::post('rooms/search', [RoomController::class,'search']);
+    Route::put('rooms/hide/{id}', [RoomController::class,'hide']);
+    //Showtime Routes
+    Route::get('showtimes/customerget', [ShowtimeController::class,'getAllShowtimesForCustomer']);
+    Route::resource('showtimes',ShowtimeController::class);
+    Route::post('showtimes/search', [ShowtimeController::class,'search']);
+    Route::put('showtimes/hide/{id}', [ShowtimeController::class,'hide']);
+    //Transaction Routes
+    Route::get('transactions/customerget', [TransactionController::class,'getAllTransactionsForCustomer']);
+    Route::resource('transactions',TransactionController::class);
+    Route::post('transactions/search', [TransactionController::class,'search']);
+    Route::put('transactions/hide/{id}', [TransactionController::class,'hide']);
+    //Reservation Routes
+    Route::get('reservations/customerget', [ReservationController::class,'getAllReservationsForCustomer']);
+    Route::resource('reservations',ReservationController::class);
+    Route::post('reservations/search', [ReservationController::class,'search']);
+    Route::put('reservations/hide/{id}', [ReservationController::class,'hide']);
+    // Auth::routes();
+    Route::resource('consume', ConsumeController::class);
 });
 
 Route::controller(LoginController::class)->group(function () {
     Route::post('login', 'login')->middleware('pass.login');
-    Route::post('register', 'register');
     Route::get('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
-
-// Route::post('/login', 'Auth\LoginController@login')->name('login');
-// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::post('/register', 'Auth\RegisterController@register');
-
 
 // Register route
 Route::post('sign-up', [RegisterController::class, 'create']);
@@ -121,7 +115,8 @@ Route::post('sign-up', [RegisterController::class, 'create']);
 Route::post('password/resent', [ForgotPasswordController::class, 'forgotPassword'])->name('password.email');
 Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'resetRequest'])->middleware('guest')->name('password.reset');
 Route::post('password/pass-reset', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
-// Route::auth();
+/
+/ Route::auth();
 Auth::routes(['verify' => true]);
 
 
@@ -129,4 +124,5 @@ Auth::routes(['verify' => true]);
 Route::middleware(['jwt.attach'])->group(function () {
     Route::view('/users', 'home');
 });
+Route::view('/sign-up', 'signup');
 Route::view('/login', 'signin')->middleware('pass.login');
